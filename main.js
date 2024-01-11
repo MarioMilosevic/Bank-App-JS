61ffb6f3846df5db197b09af6f554f805f8b0555
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-11 15:06:56 +0100

    Add data from data.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-'use strict'
-import { constants } from "./constants"
-
+"use strict";
+import { constants } from "./constants";
+import { data } from "./data";
 const {
-    welcomeBack,
-    nameInput,
-    passwordInput,
-    currentBalance,
-    navBtn,
-    transactionsDiv,
-    transferToInput,
-    transferAmountInput,
-    transferBtn,
-    loanInput,
-    loanBtn,
-} = constants()
+  welcomeBack,
+  nameInput,
+  passwordInput,
+  currentBalance,
+  navBtn,
+  transactionsDiv,
+  transferToInput,
+  transferAmountInput,
+  transferBtn,
+  loanInput,
+  loanBtn,
+} = constants();
+
+const { account1, account2, account3, account4, accounts } = data();
