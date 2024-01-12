26da58a48a94b15a7068ece1d0f3a9873c3f60b5
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-12 14:28:16 +0100

    Add in out sort

--- helpers.js
+++ helpers.js
@@ -1,19 +1,5 @@
 "use strict";
 
-// export const showMovements = (parent, movements) => {
-//   parent.innerHTML = "";
-//   movements.forEach((movement, index) => {
-//     const mov = document.createElement("div");
-//     const movementType = movement > 0 ? 'Deposit' : 'Withdrawal'
-//     mov.classList.add("movement");
-//     mov.innerHTML = `<div class="movementIndex">${index + 1}
-//     ${movement > 0  ? `<div class="movement-type-${movementType}">${movementType}</div>
-//         <div class="movementValue">${movement}$</div>
-//         `;
-//     parent.appendChild(mov);
-//   });
-// };
-
 export const showMovements = (parent, movements) => {
   parent.innerHTML = "";
   movements.forEach((movement, index) => {
