6e15be9500954b6690fec902acc3e6d2db45216e
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-14 17:19:46 +0100

    Refactor code

--- classes.js
+++ classes.js
@@ -6,8 +6,7 @@ export class Account {
     this.movements = movements;
     this.interestRate = interestRate;
     this.pin = pin;
-    // this.in = 0
-    // this.out = 0
+
     this.in = this.movements
       .filter((mov) => mov > 0)
       .reduce((acc, curr) => acc + curr, 0);
@@ -17,7 +16,6 @@ export class Account {
     this.totalBalance = this.in - Number(Math.abs(this.out));
   }
 
-  // ubacim u movements ako je vece od 0 onda depozit ako je manje onda withdrawal
 
   getMovements() {
     return this.movements;
